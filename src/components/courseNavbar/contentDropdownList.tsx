2952a1ac604dedd722a4995b5ec19de38a398822
Motaman <m.moatamen@dkilo.com>
2025-03-11 23:19:58 +0200

    add content dropdown for courseNavbar

---
+++ src/components/courseNavbar/contentDropdownList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContentDropdown from './contentDropdown'
+
+function ContentDropdownList() {
+  return (
+    <div className='flex flex-col items-center justify-center gap-3'>
+        <ContentDropdown />
+        <ContentDropdown />
+        <ContentDropdown />
+    </div>
+  )
+}
+
+export default ContentDropdownList
