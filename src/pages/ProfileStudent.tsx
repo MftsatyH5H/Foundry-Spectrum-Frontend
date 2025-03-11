310e1e8053131eab9b72b0c0cad48e60a7e05057
Motaman <momoalola681@gmail.com>
2025-03-12 00:59:32 +0200

    fix student profile

--- src/pages/ProfileStudent.tsx
+++ src/pages/ProfileStudent.tsx
@@ -1,9 +1,82 @@
 import React from 'react'
+import StudentProfile from '../components/Cards/studentprofie/StudentProfile'
 
 function ProfileStudent() {
   return (
-    <div>
-        
+    <div className='flex flex-row'>
+        <div>
+          <StudentProfile />
+        </div>
+        <div className='flex flex-col'>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+        </div>
     </div>
   )
 }
